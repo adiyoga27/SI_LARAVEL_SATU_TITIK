1dae198ec4b74bb767d0848804064e693ebb49f7
Wahyu <devcodebali@gmail.com>
2023-02-28 18:15:26 +0800

    Add Auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Web\AuthController;
+use App\Http\Controllers\Web\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::post('/auth/verify', [AuthController::class, 'verify']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', [DashboardController::class, 'index']);
 });
