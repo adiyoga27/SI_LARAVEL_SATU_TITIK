3ae037bec32ba81a2c5fab9453ff71ec7640b84f
Wahyu <devcodebali@gmail.com>
2023-05-04 01:31:52 +0800

    update resource

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -16,6 +16,17 @@ class OrderResource extends JsonResource
     {
         return [
             'order_number' => $this->order_number,
+            'customer_name' => $this->customer_name,
+            'customer_hp' => $this->customer_hp,
+            'total_price' => (integer) $this->total_price,
+            'discount' => (integer) $this->discount,
+            'tax' => (integer) $this->tax,
+            'total_payment' => (integer) $this->total_payment,
+            'status' => $this->status,
+            'payment_method' => $this->payment_method,
+            'payment_number' => $this->payment_number,
+            'note' => $this->note,
+            'uuid'=>$this->uuid,
             'cart' =>  OrderDetailResource::collection($this->details)
         ];
     }
