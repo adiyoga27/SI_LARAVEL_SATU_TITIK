bd620ea9d9714792bde38b9e8d1bc3f075ad943d
Wahyu <devcodebali@gmail.com>
2023-05-16 23:48:40 +0800

    Update status

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -150,7 +150,7 @@ class OrderController extends Controller
 
     public function order(Request $request, $uuid)
     {
-        $order = Order::where('uuid', $uuid)->where('status', 'pending')->first();
+        $order = Order::where('uuid', $uuid)->where('status', 'pending')->orWhere('status', 'progress')->first();
         if($order){
             return response()->json([
                 'status' => true,
